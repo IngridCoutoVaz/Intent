10f4083a933c7f393ad451d86dd98e7b5475b16e
IngridCoutoVaz <grindsvaz@gmail.com>
2021-05-01 15:18:52 -0300

    Atividade 28/04

--- app/src/main/java/br/edu/ifsp/scl/pdm/intent/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/pdm/intent/MainActivity.java
@@ -1,17 +1,21 @@
-package br.edu.ifsp.scl.pdm.intent;
+package br.edu.ifsp.scl.pdm.Intent;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-
-import br.edu.ifsp.scl.pdm.intent2.R;
-import br.edu.ifsp.scl.pdm.intent2.databinding.ActivityMainBinding;
+import br.edu.ifsp.scl.pdm.Intent.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     //Instancia da classede view binding
@@ -19,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
 
     //constante para passgaem de parãmetro e retorno
     public static final String PARAMETRO = "PARAMETRO";
+
+    //Request code para a MainActivity
+    private final int OUTRA_ACTIVITY_REQUEST_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,24 +77,64 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
         switch (item.getItemId()){
             case R.id.outraActivityMi:
                 //Abrir a OutraActivity
-                Intent outraActivityIntent = new Intent(this, OutraActivity.class);
+                //Intent outraActivityIntent = new Intent(this, OutraActivity.class);
+                Intent outraActivityIntent = new Intent("RECEBER_E_RETORNAR_ACTION");
 
                 //Forma 1 de passagem de parãmetros
-                Bundle parametrosBundle = new Bundle();
-                parametrosBundle.putString(PARAMETRO, activityMainBinding.parametroEt.getText().toString());
-                outraActivityIntent.putExtras(parametrosBundle);
+                //Bundle parametrosBundle = new Bundle();
+                //parametrosBundle.putString(PARAMETRO, activityMainBinding.parametroEt.getText().toString());
+                //outraActivityIntent.putExtras(parametrosBundle);
 
-                startActivity(outraActivityIntent);
+                //Forma 2 de passgaem de parâmetros
+                outraActivityIntent.putExtra(PARAMETRO, activityMainBinding.parametroEt.getText().toString());
+
+                startActivityForResult(outraActivityIntent, OUTRA_ACTIVITY_REQUEST_CODE);
 
                 return true;
             case R.id.viewMi:
+                //Abrir navegador
+                Intent abrirNavegadorIntent = new Intent(Intent.ACTION_VIEW);
+                abrirNavegadorIntent.setData(Uri.parse(activityMainBinding.parametroEt.getText().toString()));
+                startActivity(abrirNavegadorIntent);
+            case R.id.callMi:
+                //Fazer uma ligação
+                verifyCallPhonePermission();
                 return true;
         }
         return false;
+    }
 
+    private void verifyCallPhonePermission() {
+        Intent ligarIntent = new Intent(Intent.ACTION_CALL);
+        ligarIntent.setData(Uri.parse("tel: " + activityMainBinding.parametroEt.getText().toString()));
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                //Usuário já concedeu a permissão
+                startActivity(ligarIntent);
+            } else {
+                //Solicitar permissão para o usuário em tempo de de execução
+            }
+        }else{
+            //A permissão foi solicitada no Manifest
+            startActivity(ligarIntent);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == OUTRA_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK){
+            //Recebendo retorno da OutraActivity
+            Toast.makeText(this, "Voltou para MainActivity", Toast.LENGTH_SHORT).show();
+
+            String retorno = data.getStringExtra(OutraActivity.RETORNO);
+            if(retorno != null){
+                activityMainBinding.retornoTV.setText(retorno);
+            }
+        }
     }
 }
--- app/src/main/java/br/edu/ifsp/scl/pdm/intent/OutraActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/pdm/intent/OutraActivity.java
@@ -1,28 +1,39 @@
-package br.edu.ifsp.scl.pdm.intent;
+package br.edu.ifsp.scl.pdm.Intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-import br.edu.ifsp.scl.pdm.intent2.R;
-import br.edu.ifsp.scl.pdm.intent2.databinding.ActivityOutraBinding;
+import br.edu.ifsp.scl.pdm.Intent.databinding.ActivityOutraBinding;
 
-import static br.edu.ifsp.scl.pdm.intent.MainActivity.*;
+import static br.edu.ifsp.scl.pdm.Intent.MainActivity.PARAMETRO;
 
 public class OutraActivity extends AppCompatActivity {
+    //Instância da classe ViewBuilding
+    private ActivityOutraBinding activityOutraBinding;
+
+    //Constantwe para retorno para a MainActivity
+    public static final String RETORNO = "RETORNO";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        br.edu.ifsp.scl.pdm.intent2.databinding.ActivityOutraBinding activityOutraBinding = ActivityOutraBinding.inflate(getLayoutInflater());
+        activityOutraBinding = ActivityOutraBinding.inflate(getLayoutInflater());
         setContentView(activityOutraBinding.getRoot());
 
         //Recebendo parametros pela forma 1
-        Bundle parametrosBundle = getIntent().getExtras();
-        if (parametrosBundle !=null) {
-            String parametro = parametrosBundle.getString(PARAMETRO, "");
+        //Bundle parametrosBundle = getIntent().getExtras();
+        //if (parametrosBundle !=null) {
+            //String parametro = parametrosBundle.getString(PARAMETRO, "");
+            //activityOutraBinding.recebidoTV.setText(parametro);
+        //}
+
+        //Recebendo parâmetros pela forma 2
+        String parametro = getIntent().getStringExtra(MainActivity.PARAMETRO);
+        if(parametro != null){
             activityOutraBinding.recebidoTV.setText(parametro);
         }
         Log.v(getString(R.string.app_name) + "/" + getLocalClassName(), "OnCreate: Iniciando ciclo completo");
@@ -59,6 +70,11 @@ public class OutraActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
+        //Retornando par a MainActivity
+        Intent retornoIntent = new Intent();
+        retornoIntent.putExtra(RETORNO, activityOutraBinding.retornoEt.getText().toString());
+        setResult(RESULT_OK, retornoIntent);
+
         finish(); //Chama na sequência onPause, onStop, onDestroy
     }
 }
